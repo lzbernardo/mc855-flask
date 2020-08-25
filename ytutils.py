0e1259e82c83d128c0726e24d90c64792e90e749
lzbernardo <bernardo.lanza@gmail.com>
2020-08-25 11:34:58 -0300

    tmp testing

--- ytutils.py
+++ ytutils.py
@@ -1,12 +1,10 @@
 from pytube import YouTube
 import sys, os.path
 
-dir_path = os.path.dirname(os.path.abspath(__file__)) + '/videos'
-
 def download(url):
     vid =  YouTube(url).streams.filter(subtype='mp4',res='1080p')
     if(len(vid)):
-        vid.first().download(dir_path)
+        vid.first().download('/tmp')
         return 'Success'
     else:
         return 'Fail but ok'
